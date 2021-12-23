6b153618228b86408d97ff2da9b3a88375a064c9
Bruno Herfst <bruno@eness.com>
2021-12-23 11:33:11 +1100

    Add header file and start separating code

---
+++ advatek_assistor.cpp
@@ -0,0 +1,617 @@
+#include "advatek_assistor.h"
+
+bool b_refreshAdaptorsRequest = false;
+int currentAdaptor = 0;
+bool b_pollRequest = false;
+int id_networkConfigRequest = -1;
+
+std::vector<sAdvatekDevice*> devices;
+std::vector <std::string> networkAdaptors;
+
+const char* RGBW_Order[24] = {
+		"R-G-B/R-G-B-W",
+		"R-B-G/R-B-G-W",
+		"G-R-B/G-R-B-W",
+		"B-R-G/B-R-G-W",
+		"G-B-R/G-B-R-W",
+		"B-G-R/B-G-R-W",
+		"R-G-W-B",
+		"R-B-W-G",
+		"G-R-W-B",
+		"B-R-W-G",
+		"G-B-W-R",
+		"B-G-W-R",
+		"R-W-G-B",
+		"R-W-B-G",
+		"G-W-R-B",
+		"B-W-R-G",
+		"G-W-B-R",
+		"B-W-G-R",
+		"W-R-G-B",
+		"W-R-B-G",
+		"W-G-R-B",
+		"W-B-R-G",
+		"W-G-B-R",
+		"W-B-G-R"
+};
+
+const char* DriverTypes[3] = {
+		"RGB only",
+		"RGBW only",
+		"Either RGB or RGBW"
+};
+
+const char* DriverSpeedsMhz[12] = {
+	"0.4 MHz", // Data Only Slow
+	"0.6 MHz", // Data Only Fast
+	"0.8 MHz",
+	"1.0 MHz",
+	"1.2 MHz",
+	"1.4 MHz",
+	"1.6 MHz",
+	"1.8 MHz",
+	"2.0 MHz",
+	"2.2 MHz",
+	"2.5 MHz",
+	"2.9 MHz"
+};
+
+const char* TestModes[9] = {
+	"None(Live Control Data)",
+	"RGBW Cycle",
+	"Red",
+	"Green",
+	"Blue",
+	"White",
+	"Set Color",
+	"Color Fade",
+	"Single Pixel"
+};
+
+std::string macString(uint8_t * address) {
+	std::stringstream ss;
+
+	for (int i(0); i < 6; i++) {
+		ss << std::hex << std::setw(2) << static_cast<int>(address[i]);
+		if (i < 5) ss << ":";
+	}
+
+	return ss.str();
+}
+
+std::string addressString(uint8_t * address) {
+	std::stringstream ss;
+
+	for (int i(0); i < 4; i++) {
+		ss << static_cast<int>(address[i]);
+		if (i < 3) ss << ".";
+	}
+
+	return ss.str();
+}
+
+void insertSwapped16(std::vector<uint8_t> &dest, uint16_t* pData, int32_t size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		uint16_t data = pData[i];
+		dest.push_back((uint8_t)(data >> 8));
+		dest.push_back((uint8_t)data);
+	}
+}
+
+bool deviceExist(uint8_t * Mac) {
+
+	for (int d(0); d < devices.size(); d++) {
+		bool exist = true;
+		for (int i(0); i < 6; i++) {
+			if (devices[d]->Mac[i] != Mac[i]) exist = false;
+		}
+		if (exist) return true;
+	}
+
+	return false;
+}
+
+boost::asio::io_context io_context;
+boost::asio::ip::udp::endpoint receiver(boost::asio::ip::udp::v4(), AdvPort);
+boost::asio::ip::udp::socket s_socket(io_context);
+boost::asio::ip::udp::socket r_socket = boost::asio::ip::udp::socket(io_context, receiver);
+
+boost::asio::ip::tcp::resolver resolver(io_context);
+boost::asio::ip::tcp::resolver::query query(boost::asio::ip::host_name(), "");
+
+void send_udp_message(std::string ip_address, int port, bool b_broadcast, std::vector<uint8_t> message)
+{
+	s_socket.open(boost::asio::ip::udp::v4());
+	s_socket.set_option(boost::asio::socket_base::broadcast(b_broadcast));
+
+	boost::asio::ip::udp::endpoint senderEndpoint(boost::asio::ip::address::from_string(ip_address), port);
+
+	try
+	{
+		s_socket.send_to(boost::asio::buffer(message), senderEndpoint);
+		s_socket.close();
+		printf("Message sent to %s\n", ip_address.c_str());
+
+	}
+	catch (std::exception e)
+	{
+		printf("Cannot broadcast message to %s \n", ip_address.c_str());
+	}
+}
+
+void unicast_udp_message(std::string ip_address, std::vector<uint8_t> message)
+{
+	send_udp_message(ip_address, AdvPort, false, message);
+}
+
+void broadcast_udp_message(std::vector<uint8_t> message)
+{
+	send_udp_message(AdvAdr, AdvPort, true, message);
+}
+
+void updateDevice(int d) {
+	std::vector<uint8_t> dataTape;
+	dataTape.resize(12);
+	dataTape[0] = 'A';
+	dataTape[1] = 'd';
+	dataTape[2] = 'v';
+	dataTape[3] = 'a';
+	dataTape[4] = 't';
+	dataTape[5] = 'e';
+	dataTape[6] = 'c';
+	dataTape[7] = 'h';
+	dataTape[8] = 0x00;   // Null Terminator
+	dataTape[9] = 0x00;   // OpCode
+	dataTape[10] = 0x05;  // OpCode
+	dataTape[11] = 0x08;  // ProtVer
+
+	dataTape.insert(dataTape.end(), devices[d]->Mac, devices[d]->Mac + 6);
+
+	dataTape.push_back(devices[d]->DHCP);
+
+	dataTape.insert(dataTape.end(), devices[d]->StaticIP, devices[d]->StaticIP + 4);
+	dataTape.insert(dataTape.end(), devices[d]->StaticSM, devices[d]->StaticSM + 4);
+
+	dataTape.push_back(devices[d]->Protocol);
+	dataTape.push_back(devices[d]->HoldLastFrame);
+	dataTape.push_back(devices[d]->SimpleConfig);
+
+	insertSwapped16(dataTape, devices[d]->OutputPixels, devices[d]->NumOutputs);
+	insertSwapped16(dataTape, devices[d]->OutputUniv, devices[d]->NumOutputs);
+	insertSwapped16(dataTape, devices[d]->OutputChan, devices[d]->NumOutputs);
+
+	dataTape.insert(dataTape.end(), devices[d]->OutputNull, devices[d]->OutputNull + devices[d]->NumOutputs);
+	insertSwapped16(dataTape, devices[d]->OutputZig, devices[d]->NumOutputs);
+
+	dataTape.insert(dataTape.end(), devices[d]->OutputReverse, devices[d]->OutputReverse + devices[d]->NumOutputs);
+	dataTape.insert(dataTape.end(), devices[d]->OutputColOrder, devices[d]->OutputColOrder + devices[d]->NumOutputs);
+	insertSwapped16(dataTape, devices[d]->OutputGrouping, devices[d]->NumOutputs);
+
+	dataTape.insert(dataTape.end(), devices[d]->OutputBrightness, devices[d]->OutputBrightness + devices[d]->NumOutputs);
+	dataTape.insert(dataTape.end(), devices[d]->DmxOutOn, devices[d]->DmxOutOn + devices[d]->NumDMXOutputs);
+	insertSwapped16(dataTape, devices[d]->DmxOutUniv, devices[d]->NumDMXOutputs);
+
+	dataTape.push_back(devices[d]->CurrentDriver);
+	dataTape.push_back(devices[d]->CurrentDriverType);
+
+	dataTape.push_back(devices[d]->CurrentDriverSpeed);
+	dataTape.push_back(devices[d]->CurrentDriverExpanded);
+
+	dataTape.insert(dataTape.end(), devices[d]->Gamma, devices[d]->Gamma + 4);
+	dataTape.insert(dataTape.end(), devices[d]->Nickname, devices[d]->Nickname + 40);
+
+	dataTape.push_back(devices[d]->MaxTargetTemp);
+
+	unicast_udp_message(addressString(devices[d]->CurrentIP), dataTape);
+}
+
+void setTest(int d) {
+
+	std::vector<uint8_t> dataTape;
+	dataTape.resize(12);
+	dataTape[0] = 'A';
+	dataTape[1] = 'd';
+	dataTape[2] = 'v';
+	dataTape[3] = 'a';
+	dataTape[4] = 't';
+	dataTape[5] = 'e';
+	dataTape[6] = 'c';
+	dataTape[7] = 'h';
+	dataTape[8] = 0x00;   // Null Terminator
+	dataTape[9] = 0x00;   // OpCode
+	dataTape[10] = 0x08;  // OpCode
+	dataTape[11] = 0x08;  // ProtVer
+
+	dataTape.insert(dataTape.end(), devices[d]->Mac, devices[d]->Mac + 6);
+
+	dataTape.push_back(devices[d]->TestMode);
+
+	dataTape.insert(dataTape.end(), devices[d]->TestCols, devices[d]->TestCols + 4);
+
+	dataTape.push_back(devices[d]->TestOutputNum);
+
+	dataTape.push_back((uint8_t)(devices[d]->TestPixelNum >> 8));
+	dataTape.push_back((uint8_t)devices[d]->TestPixelNum);
+
+	//buf[23] = 0;// devices[d]->TestOutputNum;
+	//buf[24] = 0;//devices[d]->TestPixelNum;
+	//buf[25] = 0;//devices[d]->TestPixelNum >> 8;
+
+	unicast_udp_message(addressString(devices[d]->CurrentIP), dataTape);
+}
+
+void clearDevices() {
+	for (auto device : devices)
+	{
+		if (device)
+		{
+			delete device->Model;
+			delete device->Firmware;
+			delete[] device->OutputPixels;
+			delete[] device->OutputUniv;
+			delete[] device->OutputChan;
+			delete[] device->OutputNull;
+			delete[] device->OutputZig;
+			delete[] device->OutputReverse;
+			delete[] device->OutputColOrder;
+			delete[] device->OutputGrouping;
+			delete[] device->OutputBrightness;
+			delete[] device->DmxOutOn;
+			delete[] device->DmxOutUniv;
+			delete[] device->DriverType;
+			delete[] device->DriverSpeed;
+			delete[] device->DriverExpandable;
+			delete[] device->DriverNames;
+			delete[] device->VoltageBanks;
+			delete device;
+		}
+	}
+
+	devices.clear();
+}
+
+void setEndUniverseChannel(int startUniverse, int startChannel, int pixelCount, int outputGrouping, int &endUniverse, int &endChannel) {
+	pixelCount *= outputGrouping;
+	int pixelChannels = (3 * pixelCount); // R-G-B data
+	int pixelUniverses = ((float)(startChannel + pixelChannels) / 510.f);
+
+	endUniverse = startUniverse + pixelUniverses;
+	endChannel = (startChannel + pixelChannels - 1) % 510;
+}
+
+void bc_networkConfig(int d) {
+	std::vector<uint8_t> dataTape;
+	dataTape.resize(12);
+	dataTape[0] = 'A';
+	dataTape[1] = 'd';
+	dataTape[2] = 'v';
+	dataTape[3] = 'a';
+	dataTape[4] = 't';
+	dataTape[5] = 'e';
+	dataTape[6] = 'c';
+	dataTape[7] = 'h';
+	dataTape[8] = 0x00;   // Null Terminator
+	dataTape[9] = 0x00;   // OpCode
+	dataTape[10] = 0x07;  // OpCode
+	dataTape[11] = 0x08;  // ProtVer
+
+	dataTape.insert(dataTape.end(), devices[d]->Mac, devices[d]->Mac + 6);
+
+	dataTape.push_back(devices[d]->DHCP);
+
+	dataTape.insert(dataTape.end(), devices[d]->StaticIP, devices[d]->StaticIP + 4);
+	dataTape.insert(dataTape.end(), devices[d]->StaticSM, devices[d]->StaticSM + 4);
+
+	broadcast_udp_message(dataTape);
+}
+
+void poll() {
+
+	clearDevices();
+
+	std::vector<uint8_t> dataTape;
+	dataTape.resize(12);
+	dataTape[0] = 'A';
+	dataTape[1] = 'd';
+	dataTape[2] = 'v';
+	dataTape[3] = 'a';
+	dataTape[4] = 't';
+	dataTape[5] = 'e';
+	dataTape[6] = 'c';
+	dataTape[7] = 'h';
+	dataTape[8] = 0x00;   // Null Terminator
+	dataTape[9] = 0x00;   // OpCode
+	dataTape[10] = 0x01;  // OpCode
+	dataTape[11] = 0x08;  // ProtVer
+
+	broadcast_udp_message(dataTape);
+
+}
+
+void process_opPollReply(uint8_t * data) {
+	sAdvatekDevice * rec_data = new sAdvatekDevice();
+
+	memcpy(&rec_data->ProtVer, data, sizeof(uint8_t));
+	data += 1;
+	//std::cout << "ProtVer: " << (int)rec_data->ProtVer << std::endl;
+
+	memcpy(&rec_data->CurrentProtVer, data, sizeof(uint8_t));
+	data += 1;
+
+	memcpy(rec_data->Mac, data, 6);
+	data += 6;
+
+	memcpy(&rec_data->ModelLength, data, sizeof(uint8_t));
+	data += 1;
+
+	rec_data->Model = new uint8_t[rec_data->ModelLength];
+	memcpy(rec_data->Model, data, sizeof(uint8_t)*rec_data->ModelLength);
+	data += rec_data->ModelLength;
+
+	memcpy(&rec_data->HwRev, data, sizeof(uint8_t));
+	data += 1;
+
+	memcpy(rec_data->MinAssistantVer, data, sizeof(uint8_t) * 3);
+	data += 3;
+
+	memcpy(&rec_data->LenFirmware, data, sizeof(uint8_t));
+	data += 1;
+
+	rec_data->Firmware = new uint8_t[rec_data->LenFirmware];
+	memcpy(rec_data->Firmware, data, sizeof(uint8_t)*rec_data->LenFirmware);
+	data += rec_data->LenFirmware;
+
+	memcpy(&rec_data->Brand, data, sizeof(uint8_t));
+	data += 1;
+
+	memcpy(rec_data->CurrentIP, data, sizeof(uint8_t) * 4);
+	data += 4;
+
+	memcpy(rec_data->CurrentSM, data, sizeof(uint8_t) * 4);
+	data += 4;
+
+	memcpy(&rec_data->DHCP, data, sizeof(uint8_t));
+	data += 1;
+
+	memcpy(rec_data->StaticIP, data, sizeof(uint8_t) * 4);
+	data += 4;
+
+	memcpy(rec_data->StaticSM, data, sizeof(uint8_t) * 4);
+	data += 4;
+
+	memcpy(&rec_data->Protocol, data, sizeof(uint8_t));
+	data += 1;
+
+	memcpy(&rec_data->HoldLastFrame, data, sizeof(uint8_t));
+	data += 1;
+
+	memcpy(&rec_data->SimpleConfig, data, sizeof(uint8_t));
+	data += 1;
+
+	memcpy(&rec_data->MaxPixPerOutput, data, sizeof(uint16_t));
+	data += 2;
+	bswap_16(rec_data->MaxPixPerOutput);
+
+	memcpy(&rec_data->NumOutputs, data, sizeof(uint8_t));
+	data += 1;
+	rec_data->OutputPixels = new uint16_t[rec_data->NumOutputs];
+	memcpy(rec_data->OutputPixels, data, sizeof(uint16_t)*rec_data->NumOutputs);
+	data += (rec_data->NumOutputs * 2);
+
+	for (int i(0); i < (int)rec_data->NumOutputs; i++) {
+		bswap_16(rec_data->OutputPixels[i]);
+	}
+
+	rec_data->OutputUniv = new uint16_t[rec_data->NumOutputs];
+	memcpy(rec_data->OutputUniv, data, sizeof(uint16_t)*rec_data->NumOutputs);
+	data += (rec_data->NumOutputs * 2);
+
+	for (int i(0); i < (int)rec_data->NumOutputs; i++) {
+		bswap_16(rec_data->OutputUniv[i]);
+	}
+
+	rec_data->OutputChan = new uint16_t[rec_data->NumOutputs];
+	memcpy(rec_data->OutputChan, data, sizeof(uint16_t)*rec_data->NumOutputs);
+	data += (rec_data->NumOutputs * 2);
+
+	for (int i(0); i < (int)rec_data->NumOutputs; i++) {
+		bswap_16(rec_data->OutputChan[i]);
+	}
+
+	rec_data->OutputNull = new uint8_t[rec_data->NumOutputs];
+	memcpy(rec_data->OutputNull, data, sizeof(uint8_t)*rec_data->NumOutputs);
+	data += rec_data->NumOutputs;
+
+	rec_data->OutputZig = new uint16_t[rec_data->NumOutputs];
+	memcpy(rec_data->OutputZig, data, sizeof(uint16_t)*rec_data->NumOutputs);
+	data += (rec_data->NumOutputs * 2);
+
+	for (int i(0); i < (int)rec_data->NumOutputs; i++) {
+		bswap_16(rec_data->OutputZig[i]);
+	}
+
+	rec_data->OutputReverse = new uint8_t[rec_data->NumOutputs];
+	memcpy(rec_data->OutputReverse, data, sizeof(uint8_t)*rec_data->NumOutputs);
+	data += rec_data->NumOutputs;
+
+	rec_data->OutputColOrder = new uint8_t[rec_data->NumOutputs];
+	memcpy(rec_data->OutputColOrder, data, sizeof(uint8_t)*rec_data->NumOutputs);
+	data += rec_data->NumOutputs;
+
+	rec_data->OutputGrouping = new uint16_t[rec_data->NumOutputs];
+	memcpy(rec_data->OutputGrouping, data, sizeof(uint16_t)*rec_data->NumOutputs);
+	data += (rec_data->NumOutputs * 2);
+
+	for (int i(0); i < (int)rec_data->NumOutputs; i++) {
+		bswap_16(rec_data->OutputGrouping[i]);
+	}
+
+	rec_data->OutputBrightness = new uint8_t[rec_data->NumOutputs];
+	memcpy(rec_data->OutputBrightness, data, sizeof(uint8_t)*rec_data->NumOutputs);
+	data += rec_data->NumOutputs;
+
+	memcpy(&rec_data->NumDMXOutputs, data, sizeof(uint8_t));
+	data += 1;
+
+	memcpy(&rec_data->ProtocolsOnDmxOut, data, sizeof(uint8_t));
+	data += 1;
+
+	rec_data->DmxOutOn = new uint8_t[rec_data->NumDMXOutputs];
+	memcpy(rec_data->DmxOutOn, data, sizeof(uint8_t)*rec_data->NumDMXOutputs);
+	data += rec_data->NumDMXOutputs;
+
+	rec_data->DmxOutUniv = new uint16_t[rec_data->NumDMXOutputs];
+	memcpy(rec_data->DmxOutUniv, data, sizeof(uint16_t)*rec_data->NumDMXOutputs);
+	data += (rec_data->NumDMXOutputs * 2);
+
+	for (int i(0); i < (int)rec_data->NumDMXOutputs; i++) {
+		bswap_16(rec_data->DmxOutUniv[i]);
+	}
+
+	memcpy(&rec_data->NumDrivers, data, sizeof(uint8_t));
+	data += 1;
+
+	memcpy(&rec_data->DriverNameLength, data, sizeof(uint8_t));
+	data += 1;
+
+	rec_data->DriverType = new uint8_t[rec_data->NumDrivers];
+	memcpy(rec_data->DriverType, data, sizeof(uint8_t)*rec_data->NumDrivers);
+	data += rec_data->NumDrivers;
+
+	rec_data->DriverSpeed = new uint8_t[rec_data->NumDrivers];
+	memcpy(rec_data->DriverSpeed, data, sizeof(uint8_t)*rec_data->NumDrivers);
+	data += rec_data->NumDrivers;
+
+	rec_data->DriverExpandable = new uint8_t[rec_data->NumDrivers];
+	memcpy(rec_data->DriverExpandable, data, sizeof(uint8_t)*rec_data->NumDrivers);
+	data += rec_data->NumDrivers;
+
+	rec_data->DriverNames = new char*[rec_data->NumDrivers];
+
+	for (int i = 0; i < rec_data->NumDrivers; i++) {
+		rec_data->DriverNames[i] = new char[rec_data->DriverNameLength + 1];
+		memset(rec_data->DriverNames[i], 0, sizeof(uint8_t)*rec_data->DriverNameLength + 1);
+		memcpy(rec_data->DriverNames[i], data, sizeof(uint8_t)*rec_data->DriverNameLength);
+		data += rec_data->DriverNameLength;
+	}
+
+	memcpy(&rec_data->CurrentDriver, data, sizeof(uint8_t));
+	data += 1;
+
+	memcpy(&rec_data->CurrentDriverType, data, sizeof(uint8_t));
+	data += 1;
+
+	memcpy(&rec_data->CurrentDriverSpeed, data, sizeof(uint8_t));
+	data += 1;
+
+	memcpy(&rec_data->CurrentDriverExpanded, data, sizeof(uint8_t));
+	data += 1;
+
+	memcpy(rec_data->Gamma, data, sizeof(uint8_t) * 4);
+	data += 4;
+
+	rec_data->Gammaf[0] = (float)rec_data->Gamma[0] * 0.1;
+	rec_data->Gammaf[1] = (float)rec_data->Gamma[1] * 0.1;
+	rec_data->Gammaf[2] = (float)rec_data->Gamma[2] * 0.1;
+	rec_data->Gammaf[3] = (float)rec_data->Gamma[3] * 0.1;
+
+	memcpy(rec_data->Nickname, data, sizeof(uint8_t) * 40);
+	data += 40;
+
+	memcpy(&rec_data->Temperature, data, sizeof(uint16_t));
+	data += 2;
+	bswap_16(rec_data->Temperature);
+
+	memcpy(&rec_data->MaxTargetTemp, data, sizeof(uint8_t));
+	data += 1;
+
+	memcpy(&rec_data->NumBanks, data, sizeof(uint8_t));
+	data += 1;
+
+	rec_data->VoltageBanks = new uint16_t[rec_data->NumBanks];
+	memcpy(rec_data->VoltageBanks, data, sizeof(uint16_t)*rec_data->NumBanks);
+	data += (rec_data->NumBanks * 2);
+
+	for (int i(0); i < (int)rec_data->NumBanks; i++) {
+		bswap_16(rec_data->VoltageBanks[i]);
+	}
+
+	memcpy(&rec_data->TestMode, data, sizeof(uint8_t));
+	data += 1;
+
+	memcpy(rec_data->TestCols, data, sizeof(uint8_t) * 4);
+	data += 4;
+
+	memcpy(&rec_data->TestOutputNum, data, sizeof(uint8_t));
+	data += 1;
+
+	memcpy(&rec_data->TestPixelNum, data, sizeof(uint16_t));
+	data += 2;
+	bswap_16(rec_data->TestPixelNum);
+
+	if (!deviceExist(rec_data->Mac)) devices.emplace_back(rec_data);
+
+}
+
+void process_opTestAnnounce(uint8_t * data) {
+	return;
+}
+
+void process_udp_message(uint8_t * data) {
+	char ID[9];
+	memcpy(ID, data, sizeof(uint8_t) * 9);
+	data += 9;
+	std::cout << "ID: " << ID << std::endl;
+
+	std::string sid;
+	for (int i(0); i < 8; i++) { sid += ID[i]; }
+	if (sid.compare("Advatech") != 0) return; // Not an Advatek message ...
+
+	//--------
+
+	uint16_t OpCodes;
+	memcpy(&OpCodes, data, sizeof(uint16_t));
+	data += 2;
+	// Swap bytes
+	bswap_16(OpCodes);
+	std::cout << "messageType: " << OpCodes << std::endl;
+
+	switch (OpCodes) {
+	case OpPollReply:
+		process_opPollReply(data);
+		return;
+	case OpTestAnnounce:
+		process_opTestAnnounce(data);
+		return;
+	default:
+		return;
+	}
+}
+
+void refreshAdaptors() {
+
+	networkAdaptors.clear();
+	boost::asio::ip::tcp::resolver::iterator it;
+	try
+	{
+		it = resolver.resolve(query);
+	}
+	catch (boost::exception& e)
+	{
+		std::cout << "Query didn't resolve Any adaptors" << std::endl;
+	}
+
+	while (it != boost::asio::ip::tcp::resolver::iterator())
+	{
+		boost::asio::ip::address addr = (it++)->endpoint().address();
+		std::cout << "adaptor found: " << addr.to_string() << std::endl;
+		if (addr.is_v4()) {
+			networkAdaptors.push_back(addr.to_string());
+		}
+	}
+	if (networkAdaptors.size() > 0) {
+		adaptor_string = networkAdaptors[0];
+	}
+}
---
+++ advatek_assistor.h
@@ -0,0 +1,128 @@
+#pragma once
+
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+#include <boost/program_options.hpp>
+
+#define AdvAdr "255.255.255.255" // Advatek zero network broadcast address
+#define AdvPort 49150 // Advatek UDP Port
+#define OpPoll	0x0001 // A request for controllers to identify themselves.
+#define OpPollReply 0x0002 //Reply from a controller to a poll(includes configuration data).
+#define OpConfig  0x0005 //New configuration data being directed at a controller.
+#define OpBootload 0x0006 // Send the controller into bootloader mode.
+#define OpNetwork 0x0007 // Change network settings only.
+#define OpTestSet 0x0008 // Set the test mode.
+#define OpTestAnnounce 0x0009 // Announce the current test mode.
+#define OpVisualIdent 0x000a // A broadcast to request the controller of the designated MAC to identify its location by flashing its status LED.
+
+#define bswap_16(x) x=((((x) >> 8) & 0xff) | (((x) & 0xff) << 8))
+
+extern std::vector <std::string> networkAdaptors;
+
+extern bool b_refreshAdaptorsRequest;
+extern int currentAdaptor;
+extern bool b_pollRequest;
+extern int id_networkConfigRequest;
+
+static std::string adaptor_string = "Select Adaptor";
+
+typedef struct tAdvatekDevice {
+	uint8_t ProtVer; // Protocol version
+	uint8_t CurrentProtVer; // Using Protocol version
+	uint8_t Mac[6]; // MAC Address
+	uint8_t ModelLength; // Length of model array
+	uint8_t* Model; // Full display name
+	uint8_t HwRev; // HW rev E.G. 1.0 = 10
+	uint8_t MinAssistantVer[3]; // Minimum Advatek Assistant version required for config
+	uint8_t LenFirmware;
+	uint8_t* Firmware; // firmware version, null terminated
+	uint8_t Brand; // 0 = Advatek
+	uint8_t CurrentIP[4]; // IP address
+	uint8_t CurrentSM[4]; // Current Subnet Mask (incase DHCP issued)
+	uint8_t DHCP; // DHCP on or off
+	uint8_t StaticIP[4]; // Static IP address
+	uint8_t StaticSM[4]; // Static subnet mask
+
+	uint8_t Protocol; // 0 = sACN, 1 = artNet
+	uint8_t HoldLastFrame; // Hold last frame (timeout blank mode)
+	uint8_t SimpleConfig; // Simple = 0, or advanced config = 1
+	uint16_t MaxPixPerOutput; // Maximum number of pixels any output can drive
+	uint8_t NumOutputs; // Number of outputs (including expanded)
+
+	// NumOutputs
+	uint16_t* OutputPixels;//[2 * NumOutputs]; // Number of nodes per string
+	uint16_t* OutputUniv;//[2 * NumOutputs]; // Advanced start universes
+	uint16_t* OutputChan;//[2 * NumOutputs]; // Advanced start channels
+	uint8_t* OutputNull;//[NumOutputs]; // Null pixels
+	uint16_t* OutputZig;//[2 * NumOutputs]; // Zig zags
+	uint8_t* OutputReverse;//[NumOutputs]; // Reversed strings
+	uint8_t* OutputColOrder;//[NumOutputs]; // RGB order for each output
+	uint16_t* OutputGrouping;//[2 * NumOutputs]; // Pixel grouping
+	uint8_t* OutputBrightness;//[NumOutputs]; // Brightness limiting
+	uint8_t NumDMXOutputs; // Number of DMX outputs
+	uint8_t ProtocolsOnDmxOut;
+	uint8_t* DmxOutOn;//[NumDMXOutputs]; // DMX outputs on or off
+	uint16_t* DmxOutUniv;//[2 * NumDMXOutputs]; // Hi and Lo bytes of DMX output universes
+	uint8_t NumDrivers; // Number of pixel drivers
+	uint8_t DriverNameLength; // Length of pixel driver strings
+	uint8_t* DriverType;//[NumDrivers]; // 0 = RGB only, 1 = RGBW only, 2 = Either
+	uint8_t* DriverSpeed;//[NumDrivers]; // 0 = N/A, 1 = slow only, 2 = fast only, 3 = either, 4 = adjustable clock 0.4 - 2.9MHz(12 step)
+	uint8_t* DriverExpandable;// [NumDrivers]; // 0 = Normal mode only, 1 = capable of expanded mode
+	char** DriverNames;// [NumDrivers][LENGTH_DRIVER_STRINGS]; // Null terminated strings of driver types
+	int CurrentDriver; // Current pixel protocol selection (index)
+	uint8_t CurrentDriverType; // RGB = 0, RGBW = 1
+	int CurrentDriverSpeed; // Output chip speed (0 = Slow, 1 = Fast)
+	uint8_t CurrentDriverExpanded; // Expanded/Condensed Mode
+	uint8_t Gamma[4]; // R, G & B Gamma
+	float Gammaf[4];
+	char Nickname[40]; // if the product has a nickname, null terminatedDriverNames    uint16_t Temperature; // Hi byte of temp reading
+	uint16_t Temperature;
+	uint8_t MaxTargetTemp; // Max target temperature (fan control). 0xFF means no fan control.
+	uint8_t NumBanks; // Number of banks for voltage readings
+	uint16_t* VoltageBanks;// [NumBanks][2]; // Voltage on power banks (*10)
+	int TestMode; // Current test mode program (0 = off/live data)
+	int TestCols[4];
+	uint8_t TestOutputNum;
+	uint16_t TestPixelNum;
+} sAdvatekDevice;
+
+extern std::vector<sAdvatekDevice*> devices;
+
+std::string macString(uint8_t * address);
+std::string addressString(uint8_t * address);
+
+extern const char* RGBW_Order[24];
+extern const char* DriverTypes[3];
+extern const char* DriverSpeeds[5];
+extern const char* DriverSpeedsMhz[12];
+extern const char* TestModes[9];
+
+void insertSwapped16(std::vector<uint8_t> &dest, uint16_t* pData, int32_t size);
+
+bool deviceExist(uint8_t * Mac);
+
+extern boost::asio::io_context io_context;
+extern boost::asio::ip::udp::endpoint receiver;
+extern boost::asio::ip::udp::socket s_socket;
+extern boost::asio::ip::udp::socket r_socket;
+extern boost::asio::ip::tcp::resolver resolver;
+extern boost::asio::ip::tcp::resolver::query query;
+
+void send_udp_message(std::string ip_address, int port, bool b_broadcast, std::vector<uint8_t> message);
+void unicast_udp_message(std::string ip_address, std::vector<uint8_t> message);
+void broadcast_udp_message(std::vector<uint8_t> message);
+void updateDevice(int d);
+void setTest(int d);
+void clearDevices();
+void setEndUniverseChannel(int startUniverse, int startChannel, int pixelCount, int outputGrouping, int &endUniverse, int &endChannel);
+void bc_networkConfig(int d);
+void poll();
+void process_opPollReply(uint8_t * data);
+void process_opTestAnnounce(uint8_t * data);
+void process_udp_message(uint8_t * data);
+void refreshAdaptors();
